6aef762e625625669afa1751495a0024b42df567
Gaston Martinez Castro <gastonmartinezcastro@gmail.com>
2019-04-18 13:23:50 -0300

    Implemento registro inseguro(contraseñas en texto plano) de usuarios en la base de datos

--- registro.php
+++ registro.php
@@ -1,38 +1,59 @@
 <?php
 
-$SERVER = "localhost";
-$NAME = "gaston";
-$PASS = "1234";
-$DB = "prueba";
-
-$con = mysqli_connect($SERVER, $NAME, $PASS, $DB);
-
-if(!$con) {
-	die( 'Error de Conexion (' . mysqli_connect_errno() . ') ' . mysqli_connect_error() );
-} 
-
-$sql = "SELECT * FROM alumnos";
-
-if(!$result = mysqli_query($con, $sql)) die(mysqli_error($con));
-
-echo "<table>";
-
-for($i = 0; $i < $result->num_rows; $i++){
-	$result->data_seek($i);
-
-	$actual_data = $result->fetch_array();
-	$size_data   = sizeof($actual_data) / 2;
-	
-	echo "<tr>";
-	for($j = 0; $j < $size_data; $j++){
-
-		echo "<td>" . $actual_data[$j] . "</td>";
-
-	}
-	echo "</tr>";
+/* establecer la conexion y checkear que se haya enviado un formulario */
+if(!$_POST) {
+	die("Error, ningun formulario fue enviado");
 }
 
-echo "</table>";
-mysqli_close($con);
+$con = new mysqli("localhost", "gaston", "1234", "imusa");
+
+if($con->error) {
+	die ("Conexion abortada\n");
+}
+
+
+
+/* checkear si el mail y/o el usuario estan pickeados */
+$MAIL = $_POST["mail"];
+$USER = $_POST["username"];
+
+$query = $con->query("SELECT email, username FROM usuarios WHERE email='$MAIL' AND username='$USER'");
+
+if($con->error) {
+	die($con->error);
+}
+
+if($query->num_rows) {
+	die("Username or email already picked");
+}
+
+
+
+/* registrar al usuario */
+$PASSWORD = $_POST["password"];
+$NOMBRE = $_POST["nombre"];
+$APELLIDO = $_POST["apellido"];
+$DNI = $_POST["dni"];
+$NACIMIENTO = $_POST["nacimiento"];
+$NACIONALIDAD = $_POST["nacionalidad"];
+
+$query = "INSERT INTO usuarios (email, username, password, name, surname, dni, birthday, nationality)
+		VALUES ('$MAIL', '$USER', $PASSWORD, $NOMBRE, $APELLIDO, $DNI, $NACIMIENTO, $NACIONALIDAD)";
+
+$con->query($query) or die($con->error);
+
+echo "\nUser registered\n";
+
+
+
+/* ingresar la descripcion, si existe */
+$DESCRIPCION = $_POST["descripcion"];
+
+if($DESCRIPCION){
+	$con->query("UPDATE usuarios SET description=$DESCRIPCION WHERE username='$USER'") 
+		or die($con->error);
+}
+
+die("\nRegistro finalizado\n");
 
 ?>
