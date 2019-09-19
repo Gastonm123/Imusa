8a79fc3f1b1a5ede8219efb67e9d247ae0ea12f0
Gaston Martinez Castro <gastonmartinezcastro@gmail.com>
2019-09-19 17:31:40 -0300

    Avances en database

--- php/base_datos.php
+++ php/base_datos.php
@@ -1,16 +1,19 @@
 <?php
 
-include ''
-$servername = 'localhost';
-$username = 'poli_uno';
-$password = 'poli1';
-$db = 'poli_siete';
+include_once 'base.php';
+include_once 'usuario.php';
+include_once 'usuarioInfo.php';
 
 class Database {
     protected $conn;
+    protected $servername = 'localhost';
+    protected $username = 'poli_uno';
+    protected $password = 'poli1';
+    protected $db = 'poli_siete';
+    public $error = False;
     
     public function __create() {
-        $this->conn = new mysqli($servername, $username, $password, $db);
+        $this->conn = new mysqli($this->servername, $this->username, $this->password, $this->db);
 
         $instrucciones = [
             "CREATE TABLE users(
@@ -19,7 +22,8 @@ class Database {
                 password VARCHAR(40),
                 email    VARCHAR(20),
                 PRIMARY KEY (id),
-                UNIQUE KEY (username, email)
+                UNIQUE KEY (username),
+                UNIQUE KEY (email)
             )",
             "CREATE TABLE users_info(
                 uid INT NOT NULL,
@@ -31,19 +35,15 @@ class Database {
                 rol VARCHAR(20) DEFAULT 'user',
                 PRIMARY KEY (uid),
                 UNIQUE KEY (uid)
-            )",
-            "INSERT INTO users 
-                (username, password, email) VALUES 
-                ('admin', SHA1('admin'), 'admin'
-            )",
-            'INSERT INTO users_info (uid) VALUES ("1")'
+            )"
         ];
 
         foreach ($instrucciones as $instruccion) {
             $result = $this->conn->query($instruccion);
 
             if ($result == FALSE) {
-                throw new Error('No se pudo inicializar la base de datos correctamente');
+                var_dump($instruccion);
+                $this->error = True;
             }
         }
     }
@@ -54,10 +54,65 @@ class Database {
         }
 
         $id = $usuario->id;
-        $sql = 'UPDATE users SET () WHERE id = '
+        $sql = 'UPDATE users SET () WHERE id = ';
     }
 
     public function actualizarUserInfo ($usuario) {
 
+    }
+
+    public function crearUser (Usuario $user) {
+        // crear una entrada para users y una para users_info
+        $sql = $this->insertString($user, 'users');
+        $result = $this->conn->query($sql);
+
+        if ($result == FALSE) {
+            $this->error = True;
+            echo 'Error creando el usuario';
+            return;
+        }
+
+        $id = $this->lastId();
+        $sql = "INSERT INTO users_info (uid) VALUES ($id)";
+        $result = $this->conn->query($sql);
+
+        if ($result == FALSE) {
+            $this->error = True;
+            echo 'Error creando la informacion del usuario';
+            return;
+        }
+    }
+
+    private function lastId() {
+        $result = $this->conn->query('SELECT LAST_INSERT_ID()');
+        $arr = $result->fetch_assoc();
+
+        return $arr;
+    }
+
+    private function insertString (Base $object, $table) {
+        $values = '';
+        $keys = '';
+
+        foreach ($object as $key => $value) {
+            if (isempty($value)) {
+                continue;
+            }
+
+            if ($key == 'password') {
+                $values .= "SHA1('$values'),";
+            } else {
+                $values .= "'$values',";
+            }
+
+            $keys .= "$key,";
+        }
+
+        $values = rtrim($values, ',');
+        $keys = rtrim($keys, ',');
+
+        $sql = "INSERT INTO $table ($keys) VALUES ($values)";
+
+        return $sql;
     }
 }
